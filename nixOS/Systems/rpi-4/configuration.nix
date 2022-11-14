1a170fa42a0d5625d529efe003dde78f6ad67058
= <ido.samuelson@gmail.com>
2022-11-14 00:45:26 -0600

    Initial Commit

---
+++ nixOS/Systems/rpi-4/configuration.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  networking.hostName = "allskycamera";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "ter-powerline-v32n";
+    useXkbConfig = true;
+    packages = [ pkgs.terminus_font pkgs.powerline-fonts ];
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  users.users.fisheye = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "sky360";
+    packages = with pkgs; [
+      indi-full
+    ];
+    shell = pkgs.fish;
+  };
+  
+  users.defaultUserShell = pkgs.fish;
+   
+  environment.sessionVariables = rec {
+    TERM="iterm";
+  };
+
+  environment.systemPackages = with pkgs; [
+    helix rnix-lsp nixpkgs-fmt
+    btop
+    lsd exa bat wget neofetch mcfly direnv
+    ranger mc
+    git gitui tig gh
+    usbutils
+  ];
+
+  programs.fish.enable = true;
+  services.openssh.enable = true;
+  system.stateVersion = "22.11";
+}
